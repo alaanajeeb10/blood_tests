7c8d5d70d517ef578155cd4e8fa6ba9e59b08bf7
alaanajeeb10 <mhmdallaalden@gmail.com>
2025-03-06 09:06:26 +0200

    ADD a Get tests function main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,22 @@ var URL = "http://localhost:6969";
 var hosts = [];
 var selectedHostId = null; // מזהה המדידה שנבחרה לעריכה
 
+async function GetTests(hostId) {
+    try {
+        let url = `${URL}/T/?host_id=${hostId}`;
+        let response = await fetch(url);
+        let reply = await response.json();
+
+        let filteredData = reply.data.filter(test => test.host_id == hostId);
+
+        CreateTableBody(filteredData);
+    } catch (error) {
+        console.error("Error fetching measurements:", error);
+    }
+}
+
+
+
 async function GetHosts() {
     try {
         let url = `${URL}/H/`;
