a5c16ab9e93d7cdb9a3a19bd08310833042ff868
alaanajeeb10 <mhmdallaalden@gmail.com>
2025-03-06 08:29:40 +0200

    // Add measurement (POST)

---
+++ Tests_R.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const Tests_Mid = require("../middleware/Tests_Mid");
+const {Router} = require("express");
+
+// Add measurement (POST)
+router.post('/', [Tests_Mid.Addtests], (req, res) => {
+    if (req.success) {
+        res.status(200).json({ msg: "ok", Last_Id: req.insertId });
+    } else {
+        console.error("Error adding measurement:", req.error);
+        res.status(500).json({ message: req.error || "Unknown error" });
+    }
+});
