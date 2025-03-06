9a7fea7bd5b5894220d08b3c1cc6696cc33299c2
alaanajeeb10 <mhmdallaalden@gmail.com>
2025-03-06 07:53:18 +0200

    הוספת קישור ההפעלה של host

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // npm i express body-parser ejs htmlspecialchars mysql2  slashes@2.0.0
-const port = 2135;
+const port = 6969;
 const express = require('express');
 const app = express();
 app.use(express.json());
@@ -13,3 +13,8 @@ global.db_pool = db_M.pool;
 
 global.htmlspecialchars = require('htmlspecialchars');
 // const { addSlashes, stripSlashes } = require('slashes');
+
+
+app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+    console.log(`Now listening on port http://localhost:${port}`);
+});
