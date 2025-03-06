2296e42e6cb2d70ebdb6c9764d55d1ec460767a4
alaanajeeb10 <mhmdallaalden@gmail.com>
2025-03-06 08:30:18 +0200

    // Add tests (POST)

--- Tests_R.js
+++ Tests_R.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Tests_Mid = require("../middleware/Tests_Mid");
 const {Router} = require("express");
 
-// Add measurement (POST)
+
 router.post('/', [Tests_Mid.Addtests], (req, res) => {
     if (req.success) {
         res.status(200).json({ msg: "ok", Last_Id: req.insertId });
